eea122e7077544a9f74594ba40321a4614a07d30
jdselby24 <jdselby24@gmail.com>
2019-11-14 13:12:11 +0000

    Added doc blocks to TokenModel

--- src/classes/models/db/TokenModel.php
+++ src/classes/models/db/TokenModel.php
@@ -4,21 +4,48 @@ namespace Slacker\models\db;
 
 use PDO;
 
+/**
+ * Class for token based interaction
+ */
 class TokenModel
 {
+
+    /**
+     * Database object
+     *
+     * @var PDO
+     */
     private $db;
 
+    /**
+     * Constructor for injecting PDO object
+     *
+     * @param PDO $db Database object
+     */
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
-
+    
+    /**
+     * For generating a new login token
+     *
+     * @param string $user Username to generate token for 
+     * @return string Random MD5 hash based on usename, timestamp and a random number
+     */
     public function generate_login_token(string $user) : string {
         $string = $user . time() . rand();
         $token = md5($string, false);
         return $token;
     }
-
+    
+    /**
+     * Adds a users token to the database
+     *
+     * @param string $user Users username
+     * @param string $token Users current token
+     * @return boolean DB Success
+     */
     public function set_token_active(string $user,string $token) : bool {
 
         $tokenAddQuery = "INSERT INTO `users_active` (`uname`,`token`) VALUES (:user, :token)";
@@ -29,6 +56,13 @@ class TokenModel
         return $success;
     }
 
+    /**
+     * Set a users past token to inactive
+     *
+     * @param string $user Users username
+     * @param string $token Users current token
+     * @return boolean DB Success
+     */
     public function set_token_inactive(string $user, string $token) : bool {
 
         $tokenInactiveQuery = "UPDATE `users_active` SET `ended` = '1', `time_out` = :timeval WHERE `uname` = :user AND `token` = :token";
@@ -40,6 +74,12 @@ class TokenModel
         return $success;
     }
 
+    /**
+     * Sets all a users tokens to inactive
+     *
+     * @param string $user Users username
+     * @return boolean DB Success
+     */
     public function set_token_inactive_all(string $user) : bool {
 
         $tokenInactiveQuery = "UPDATE `users_active` SET `ended` = '1', `time_out` = :timeval WHERE `uname` = :user;";
@@ -50,6 +90,12 @@ class TokenModel
         return $success;
     }
 
+    /**
+     * Get a users current token
+     *
+     * @param string $user Users username
+     * @return string DB Success
+     */
     public function get_current_token(string $user) : string {
 
         $tokenQuery = "SELECT `token` FROM `users_active` WHERE NOT `ended` = '1' AND `uname` = :user";
@@ -60,6 +106,13 @@ class TokenModel
         return $token;
     }
     
+    /**
+     * For updating a users token in the DB
+     *
+     * @param string $oldToken Users current token
+     * @param string $newToken Newly generated tokeb
+     * @return boolean DB Success
+     */
     public function update_token(string $oldToken, string $newToken) : bool {
         $updateTokenQuery = "UPDATE `users_active` SET `token` = :newtoken WHERE `token` = :oldtoken AND NOT `ended` = '1';";
         $query = $this->db->prepare($updateTokenQuery);
@@ -68,7 +121,13 @@ class TokenModel
         $state = $query->execute();
         return $state;
     }
-    
+
+    /**
+     * Get all token data
+     *
+     * @param string $token 
+     * @return array Token data
+     */
     public function get_token_data(string $token) : array {
     
         $tokenQuery = "SELECT * FROM `users_active` WHERE `token` = :token";
