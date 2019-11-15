12ec3231cfc2b881b0701992314c304374a185b5
jdselby24 <jdselby24@gmail.com>
2019-11-15 09:00:03 +0000

    Completed login controller

--- src/classes/controllers/user/LoginController.php
+++ src/classes/controllers/user/LoginController.php
@@ -7,25 +7,23 @@ use Slim\Http\Response;
 
 use Slacker\models\db\UserModel;
 use Slacker\models\db\TokenModel;
+use Slacker\models\db\MessageModel;
 
 class LoginController
 {
     private $UserModel;
     private $TokenModel;
+    private $MessageModel;
 
-    public function __construct(UserModel $userModel, TokenModel $tokenModel)
+    public function __construct(UserModel $userModel, TokenModel $tokenModel, MessageModel $messageModel)
     {
         $this->UserModel = $userModel;
         $this->TokenModel = $tokenModel;
+        $this->MessageModel = $messageModel;
     }
 
     public function __invoke(Request $request, Response $response, array $args) : Response
     {
-        $res = [
-            'success' => false,
-            'message' => 'data received'
-        ];
-
         $req_data = $request->getParsedBody();
 
         if(isset($req_data['username']) && isset($req_data['password'])) {
@@ -38,8 +36,17 @@ class LoginController
 
         if(password_verify($password, $passHash)) {
             // TODO: Finish transfering functionality, add message functions to a MessageModel class
+            $token = $this->TokenModel->generate_login_token($username);
+            $token_added = $this->TokenModel->set_token_inactive($username, $token);
+        } else {
+            return $response->withJson(['success' => false, 'message' => 'Incorrect password', 'data' => $request], 200);
         }
 
-
+        if($token_added === true) {
+            $this->MessageModel->add_msg('Slackerbot', "User $username logged in", 1);
+            return $response->withJson(['success' => true, 'message' => 'Login Success', 'token'=>$token, 'data' => $request], 200);
+        } else {
+            return $response->withJson(['success' => false, 'message' => 'Internal Error: Could not log user in', 'data' => $request], 500);
+        }
     }
 }
